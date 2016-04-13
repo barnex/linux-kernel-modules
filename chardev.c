c326eb2e90a971346e6e2eb6bcfaf0964709f2a6
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-04-13 21:01:43 +0100

    register device class

--- chardev.c
+++ chardev.c
@@ -1,5 +1,5 @@
 //#include <asm/uaccess.h>
-//#include <linux/device.h>
+#include <linux/device.h>
 #include <linux/fs.h>
 //#include <linux/init.h>
 //#include <linux/kernel.h>
@@ -17,8 +17,8 @@ static int majorNumber; // device number
 static char message[256];
 static int len_message;
 static int countOpens;
-static struct class* drvClass = NULL;
-static struct device* drvDevice = NULL;
+static struct class* myClass = NULL;
+static struct device* myDevice = NULL;
 
 static int dev_open(struct inode *inodep, struct file *filep){
 	return 0;
@@ -49,10 +49,20 @@ static int __init chardev_init(void){
 
 	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
 	if(majorNumber < 0){
-		printk(KERN_ALERT "Arne's character device: failed to register\n");
+		printk(KERN_ALERT "Arne's character device: register_chrdev failed\n");
 		return majorNumber;	
 	}
-	printk(KERN_INFO "Arne's character device: registered at major number %d\n", majorNumber);
+	printk(KERN_INFO "Arne's character device: register_chrdev: %d\n", majorNumber);
+
+	myClass = class_create(THIS_MODULE, CLASS_NAME);
+	if (IS_ERR(myClass)){
+		class_destroy(myClass);
+		unregister_chrdev(majorNumber, DEVICE_NAME);	
+		printk(KERN_ALERT "Arne's character device: device_create failed\n");
+		return PTR_ERR(myClass);
+	}
+	printk(KERN_INFO "Arne's character device: class_create: OK\n");
+
 
 
 	return 0;
