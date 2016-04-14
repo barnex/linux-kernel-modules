834f231806b9ed3fe7a7853ea9eef237f8fc6f59
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-04-14 21:32:47 +0100

    start testing

---
+++ test/hellodev_test.go
@@ -0,0 +1,21 @@
+package hellodev
+
+import (
+	"os"
+	"testing"
+)
+
+const dev = "/dev/hello"
+
+func TestOpenClose(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		f, err := os.Open(dev)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err2 := f.Close()
+		if err2 != nil {
+			t.Fatal(err2)
+		}
+	}
+}
