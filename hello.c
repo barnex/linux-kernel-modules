32c9853b521773cc523a6e7f24738d65c2c92c4c
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-04-16 11:40:49 +0100

    module exit

--- hello.c
+++ hello.c
@@ -1,1 +1,18 @@
 #include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+#define NAME "hello"
+
+static int __init mod_init(void) {
+	printk(KERN_INFO "%s: init\n", NAME);
+	return 0;
+}
+
+static void __exit mod_exit(void) {
+	printk(KERN_INFO "%s: exit\n", NAME);
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
